adf5b04db35b937751f3b9169c55b4b806901e2e
wqf <m13521139180@163.com>
2018-03-30 10:31:54 +0800

    Initial Commit

---
+++ KVO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KVO
+//
+//  Created by wqf on 2018/3/30.
+//  Copyright © 2018年 wqf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KVO/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  KVO
+//
+//  Created by wqf on 2018/3/30.
+//  Copyright © 2018年 wqf. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
